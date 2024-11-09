7b63be7183ae704959ba132b1f0ae4e997ff0fbb
ArsonivAlt <c-h09@outlook.com>
2024-11-09 19:22:02 +1300

    made win& rounded

--- index.js
+++ index.js
@@ -627,7 +627,7 @@ client.on("message", (channel, userstate, message, self) => {
         const matches = data.data.statistics.season.playedMatches.ranked;
         const bestWS = data.data.statistics.total.highestWinStreak.ranked;
         const winrate = matches > 0 ? ((wins / matches)*100) : 0;
-        client.say(channel, `${args[1]}'s elo: ${eloRate} | #${eloRank} | ${winrate}% (${wins}W - ${losses}L) | Matches: ${matches} | Best WS: ${bestWS}`);
+        client.say(channel, `${args[1]}'s elo: ${eloRate} | #${eloRank} | ${Math.floor(winrate)}% (${wins}W - ${losses}L) | Matches: ${matches} | Best WS: ${bestWS}`);
       } catch (error) {
         console.error("Fetch error:", error);
         client.say(
